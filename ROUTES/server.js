31a2a421eaf361f9b584430406dfb5616ee9c115
Phillip48 <phillippereira02@gmail.com>
2022-04-28 16:06:32 -0400

    Created and styled notes taking page

--- ROUTES/server.js
+++ ROUTES/server.js
@@ -11,8 +11,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+
 app.use('/api', apiRoutes);
+
 app.use('/', htmlRoutes);
+
+
+
+
 
 // start the server on the port
 app.listen(PORT, () => console.log(`This port is listening at ${PORT}`));
