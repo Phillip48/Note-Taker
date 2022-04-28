a70db384d1cd3d219ba5bd62ba192a610cb1eb67
Phillip48 <phillippereira02@gmail.com>
2022-04-27 23:41:45 -0400

    Finished starter code and added some very basic HTML

---
+++ assets/db/store.js
@@ -0,0 +1,20 @@
+const util = require('util');
+const fs = require('fs');
+
+const uuidv1 = require('uuid/v1');
+
+class Store {
+    read () {
+
+    }
+
+    write () {
+
+    }
+
+    // get notes
+    // add notes
+    // delete notes
+}
+
+module.exports = new Store();
